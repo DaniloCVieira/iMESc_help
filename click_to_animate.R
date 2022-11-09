65bd509af28ee360a21f130c7b41c9e1fc8aca8a
Danilo Vieira <vieiradc@yahoo.com.br>
2022-11-09 06:01:39 -0300

    3th commit

---
+++ click_to_animate.R
@@ -0,0 +1,7 @@
+
+<label class="click-to-gif" title="click/hit space to show gif">
+  <input type="checkbox">
+    <img src="gif/Create%20a%20Datalist0.gif"
+    alt="Captain America saying he can do this all day - animated"
+    width="500" height="180">
+      </label>
